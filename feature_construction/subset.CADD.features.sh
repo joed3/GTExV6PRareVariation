b774e0d4c0f5907afd54ad0d12368d5fca69d9a9
Emily Tsang <etsang@stanford.edu>
2016-08-31 16:29:43 -0700

    added indentation and line breaks to make it easier to read

--- feature_construction/subset.CADD.features.sh
+++ feature_construction/subset.CADD.features.sh
@@ -2,6 +2,8 @@
 
 # Chrom, Pos0, Pos1, CpG, priPhCons, mamPhCons, verPhCons, priPhyloP, mamPhyloP, verPhyloP, GerpN, GerpS, GerpRS, GerpRSpval, fitCons, TFBS, TFBSPeaks, TFBSPeaksMax
 
-zcat $CADD_DIR/whole_genome_SNVs_inclAnno.tsv.gz | tail -n+3 | awk 'BEGIN{OFS="\t"}{print "chr"$1,$2-1,$2,$15,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$39,$79,$80,$81}' | uniq | \
-sort -k1,1 -k2,2n --parallel=8 | bgzip -c > ${CADD_DIR}/sorted.consolidated.annotations.bed.gz
+zcat $CADD_DIR/whole_genome_SNVs_inclAnno.tsv.gz | tail -n+3 | \
+	awk 'BEGIN{OFS="\t"}{print "chr"$1,$2-1,$2,$15,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$39,$79,$80,$81}' | uniq | \
+	sort -k1,1 -k2,2n --parallel=8 | bgzip -c > ${CADD_DIR}/sorted.consolidated.annotations.bed.gz
+
 tabix -p bed ${CADD_DIR}/sorted.consolidated.annotations.bed.gz
