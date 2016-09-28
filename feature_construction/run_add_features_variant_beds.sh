05fde577a6437ed28b8b256d63760e4d8b34fa9d
Emily Tsang <etsang@stanford.edu>
2016-09-28 14:08:51 -0700

    changed variable name

--- feature_construction/run_add_features_variant_beds.sh
+++ feature_construction/run_add_features_variant_beds.sh
@@ -4,11 +4,11 @@ set -o nounset -o errexit -o pipefail
 
 ## run add_features_variant_beds.sh in batches
 
-NCORES=15
+nproc=15
 dir=${RAREVARDIR}/features
 
-parallel --jobs $NCORES --xapply ./add_features_variant_beds.sh ::: ${dir}/variantBeds/individuals/withCADD/*_SNPs.CADD.bed.gz ::: ${dir}/variantBeds/1KG/SNPs.1kg.AF.bed.gz ::: ${dir}/bySite
+parallel --jobs $nproc --xapply ./add_features_variant_beds.sh ::: ${dir}/variantBeds/individuals/withCADD/*_SNPs.CADD.bed.gz ::: ${dir}/variantBeds/1KG/SNPs.1kg.AF.bed.gz ::: ${dir}/bySite
 
-parallel --jobs $NCORES --xapply ./add_features_variant_beds.sh ::: ${dir}/variantBeds/individuals/*_indels.bed.gz ::: ${dir}/variantBeds/1KG/indels.1kg.AF.bed.gz ::: ${dir}/bySite
+parallel --jobs $nproc --xapply ./add_features_variant_beds.sh ::: ${dir}/variantBeds/individuals/*_indels.bed.gz ::: ${dir}/variantBeds/1KG/indels.1kg.AF.bed.gz ::: ${dir}/bySite
 
 wait
