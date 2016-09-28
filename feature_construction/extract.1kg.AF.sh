fd63c208c752cce7188486848910a06ae46c833a
Emily Tsang <etsang@stanford.edu>
2016-09-28 14:05:28 -0700

    set number of processes at top of script instead of in the parallel commands

--- feature_construction/extract.1kg.AF.sh
+++ feature_construction/extract.1kg.AF.sh
@@ -2,6 +2,9 @@
 
 set -o nounset
 
+# set number of processes
+nproc=15
+
 # get AF of the 5 superpopulations from the 1kg vcfs
 
 # for each chromosome, extract the AF information from the vcf
@@ -24,10 +27,10 @@ export vcfdir
 export outdir
 
 for i in chr{1..22}; do echo $i; done | \
-parallel --jobs 15 "vcftools --gzvcf ${vcfdir}/ALL.{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --out ${outdir}/{}.indels --keep-only-indels --positions $gtexindel --get-INFO EAS_AF --get-INFO AMR_AF --get-INFO AFR_AF --get-INFO EUR_AF --get-INFO SAS_AF"
+parallel --jobs $nproc "vcftools --gzvcf ${vcfdir}/ALL.{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --out ${outdir}/{}.indels --keep-only-indels --positions $gtexindel --get-INFO EAS_AF --get-INFO AMR_AF --get-INFO AFR_AF --get-INFO EUR_AF --get-INFO SAS_AF"
 
 for i in chr{1..22}; do echo $i; done | \
-parallel --jobs 15 "vcftools --gzvcf ${vcfdir}/ALL.{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --out ${outdir}/{}.SNPs --remove-indels --positions $gtexsnp --get-INFO EAS_AF --get-INFO AMR_AF --get-INFO AFR_AF --get-INFO EUR_AF --get-INFO SAS_AF"
+parallel --jobs $nproc "vcftools --gzvcf ${vcfdir}/ALL.{}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz --out ${outdir}/{}.SNPs --remove-indels --positions $gtexsnp --get-INFO EAS_AF --get-INFO AMR_AF --get-INFO AFR_AF --get-INFO EUR_AF --get-INFO SAS_AF"
 
 cat ${outdir}/*indels.INFO | ./process.1kg.AF.py | sort -k1,1 -k2,2n > ${outdir}/indels.1kg.AF.bed
 
