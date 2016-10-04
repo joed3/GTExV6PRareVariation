3a836c2a90321c68576b6ab5df2924f438a4000f
Emily Tsang <etsang@stanford.edu>
2016-10-04 16:49:05 -0700

    simplified paths that need to be modified

--- paper_figures/suppfig.number.rare.vars.pca.R
+++ paper_figures/suppfig.number.rare.vars.pca.R
@@ -2,9 +2,8 @@
 
 ## PATHS TO BE SET ###########
 dir = Sys.getenv('RAREVARDIR')
-subjectFile = '/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2015-01-12/sample_annotations/GTEx_Analysis_2015-01-12_\
-Annotations_SubjectPhenotypesDS.txt' # adjust path as necessary
-covariatesDir = '/mnt/lab_data/montgomery/shared/datasets/gtex/GTEx_Analysis_2015-01-12/eqtl_data/eQTLInputFiles/covariates/'
+subjectFile = 'GTEx_Analysis_2015-01-12_Annotations_SubjectPhenotypesDS.txt' # adjust path as necessary
+covariatesDir = 'eQTLInputFiles/covariates/'
 
 # looking into whether or not individuals with a lot of rare variants are outliers in PCA space
 
