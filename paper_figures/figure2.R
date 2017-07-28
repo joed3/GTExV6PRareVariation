c83e4189714d5413ecc1bd8d6db328dd33bc0996
Emily Tsang <etsang@stanford.edu>
2017-07-28 14:05:02 -0700

    fixed variable name after updating script 2a much earlier

--- paper_figures/figure2.R
+++ paper_figures/figure2.R
@@ -22,7 +22,7 @@ fontsizes = theme(axis.text = element_text(size = fontsize),
                   panel.grid.minor = element_blank())
 
 # make uniform format
-count.ratio.plot.withPC = count.ratio.plot.withPC + fontsizes
+count.ratio.plot = count.ratio.plot + fontsizes
 
 props.plot.medz.singlez = props.plot.medz.singlez + fontsizes + theme(legend.position = c(0.75, 0.85))
 
@@ -30,7 +30,7 @@ ase.plot = ase.plot + fontsizes +
     theme(plot.margin = unit(c(5.5,5.5,-5,5.5), "pt"))
 
 figure2.combined = ggdraw() +
-    draw_plot(count.ratio.plot.withPC, 0.08, 0.55, 0.82, 0.45) +
+    draw_plot(count.ratio.plot, 0.08, 0.55, 0.82, 0.45) +
     draw_plot(props.plot.medz.singlez, 0, 0, 0.55, 0.53) +
     draw_plot(ase.plot, 0.55, 0, 0.45, 0.53) +
     draw_plot_label(c('a','b','c'), c(0.1, 0, 0.55), c(1, 0.57, 0.57), size = 16.8)
